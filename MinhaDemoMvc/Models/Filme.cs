cd90f1a8e2e4f1a00693d7eaac44487561467141
Rainer <rainermr89@hotmail.com>
2022-05-05 18:32:22 -0300

    Trabalhando com DataAnnotations

--- MinhaDemoMvc/Models/Filme.cs
+++ MinhaDemoMvc/Models/Filme.cs
@@ -1,14 +1,35 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace MinhaDemoMvc.Models
 {
     public class Filme
     {
+        [Key]
         public int Id { get; set; }
+        
+        [Required(ErrorMessage = "O campo Título é obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O título precisa ter entre 3 ou 60 caracteres")]
         public string Titulo { get; set; }
-        public DateTime DataLancamento { get; set; }
+
+        [DataType(DataType.DateTime, ErrorMessage = "Data em formato incorreto")]
+        [Required(ErrorMessage = "O campo Data de Lançamento é obrigatório")]
+        [Display(Name = "Data de Lançamento")]
+        public DateTime DataLancamento { get; set; }  
+        
+        [StringLength(30, ErrorMessage = "Máximo de 30 caracteres"), Required(ErrorMessage = "O campo gênero é requerido")]
         public string Genero { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Valor de 1 a 1000")]
+        [Required(ErrorMessage = "Preencha o campo Valor")]
+        [Column(TypeName = "decimal(18,2")]
         public decimal Valor { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Avaliação")]
+        [Display(Name = "Avaliação")]
+        [RegularExpression(@"^[0-5]*$", ErrorMessage = "Somente números")]
         public int Avaliacao { get; set; }
     }
 }
